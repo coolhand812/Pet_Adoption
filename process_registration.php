87bd1023ef34411fa3bf12a80221410c4c20bf6c
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2018-06-05 22:53:29 -0500

    Chnages to added php code to p_r & changed form info on reg_html

--- process_registration.php
+++ process_registration.php
@@ -1,1 +1,91 @@
-//registration
+<html>
+	<head>
+		<title>Joe's Website</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+
+	<body>
+		<div id="container">
+			<div id="header">
+				<h1>Joe's Website</h1>
+			</div>
+			
+			<div id="content">
+				<div id="nav">
+					<h3>Navigation</h3>
+					<ul>
+						<li>Home</li>
+						<li>About</li>
+						<li>Contact</li>
+					</ul>
+				</div>
+				
+				<div id="header2">
+					<h2>Pet Adoption Form</h2>
+				</div>
+				
+				<div id="main">
+                
+					<?php
+						// define variables and set to empty values
+						$petIDErr = $petNameErr = $genderErr = $pet_typeErr = $ageErr = $notesErr = "";
+						$petID = $petName = $gender = $pet_type = $age = $notes = "";
+					
+						if ($_SERVER["REQUEST_METHOD"] == "POST") {
+					  		if (empty($_POST["name"])) {
+								$nameErr = "Name is required";
+					  		} else {
+								$name = test_input($_POST["name"]);
+								// check if name only contains letters and whitespace
+							if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+						  		$nameErr = "Only letters and white space allowed"; 
+								}
+					  		}
+					  
+					  		if (empty($_POST["email"])) {
+								$emailErr = "Email is required";
+					  		} else {
+								$email = test_input($_POST["email"]);
+								// check if e-mail address is well-formed
+							if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+						  		$emailErr = "Invalid email format"; 
+								}
+					  		}
+						
+					  		if (empty($_POST["website"])) {
+								$website = "";
+					  		} else {
+								$website = test_input($_POST["website"]);
+								// check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+							if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+						  		$websiteErr = "Invalid URL"; 
+								}
+					  		}
+					
+					  		if (empty($_POST["comment"])) {
+								$comment = "";
+					  		} else {
+								$comment = test_input($_POST["comment"]);
+					  		}
+					
+					  		if (empty($_POST["gender"])) {
+								$genderErr = "Gender is required";
+					  		} else {
+								$gender = test_input($_POST["gender"]);
+					  		}
+						}
+					?>	
+					
+				</div>
+				
+			</div>
+			
+			<div id="footer">
+				Copyright &copy; 2018 Joe Diaz
+			</div>
+			
+		</div>
+		
+	</body>
+	
+</html>
