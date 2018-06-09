d6fc3f96cc975317f65cadc0d316ef2fb2b0cc41
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2018-06-09 17:01:36 -0500

    fixed the home and about links

---
+++ about.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+		<title>Joe's Website</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+
+	<body>
+		<div id="container">
+			<div id="header">
+				<h1>Joe's Website</h1>
+			</div>
+			
+			<div id="content">
+				<div id="nav">
+					<h3>Navigation</h3>
+					<ul>
+						<li><a href="main_menu.html">Main</a></li>
+						<li>About</li>
+						<li>Contact</li>
+					</ul>
+				</div>
+				
+				<div id="header2">
+					<h2>Pet Adoption</h2>
+				</div>
+				
+				<div id="main">
+						
+				//input information about pet adoption	
+					
+				</div>
+				
+			</div>
+			
+			<div id="footer">
+				Copyright &copy; 2018 Joe Diaz
+			</div>
+			
+		</div>
+		
+	</body>
+	
+</html>
