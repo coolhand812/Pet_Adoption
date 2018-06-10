f5b7bb4dfeb5f1ce2395cbca83beb02cef674557
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2018-06-09 20:55:27 -0500

    set variable to allow for first fill of database

--- process_registration.php
+++ process_registration.php
@@ -37,28 +37,38 @@
 								$inDate = $_POST["inDate"];
 							}
 
-							//petID validate If the pet_id already exists
-							$servername = "localhost1234";
-							$username = "root";
-							$password = "";
-							$database = "pet_adoption";
+							$noEntries = 0;
+							
+							if($noEntries != 0){
+								//petID validate If the pet_id already exists
+								$servername = "localhost1234";
+								$username = "root";
+								$password = "";
+								$database = "pet_adoption";
 
-							$conn = new mysqli($servername, $username, $password, $database);
-							// Check connection
-							if (!$conn)
-							{
-								die("Connection failed: " . mysqli_connect_error());
+								$conn = new mysqli($servername, $username, $password, $database);
+								// Check connection
+								if (!$conn)
+								{
+									die("Connection failed: " . mysqli_connect_error());
+								}
+								echo "<b>Connection to MySQL DB established!</b> <br>";
+								$result = $conn->query("SELECT pet_id FROM pettable WHERE pet_id = $petID");
+								if($result->num_rows == 0) {
+									 // row not found, do stuff...
+								 	$petID = $_POST["petID"];
+								} else {
+									// do other stuff...
+									$petIDErr = "Please choose a different pet ID";
+								}
+								$conn->close();
+							}else{
+								if(isset($_POST["petID"]))
+								{
+									$petID = $_POST["petID"];
+								}
+								$noEntries++;
 							}
-							echo "<b>Connection to MySQL DB established!</b> <br>";
-							$result = $conn->query("SELECT pet_id FROM pettable WHERE pet_id = $petID");
-							if($result->num_rows == 0) {
-								 // row not found, do stuff...
-								 $petID = $_POST["petID"];
-							} else {
-								// do other stuff...
-								$petIDErr = "Please choose a different pet ID";
-							}
-							$conn->close();
 
 							if (empty($_POST["petName"])) {
 									$petNameErr = "Name is required";
